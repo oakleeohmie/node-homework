ada113f7b1f6ad8b890ee35fee019c8bbef688d4
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-01-29 22:26:15 -0700

    created an html file, not sure how to turn it into a pdf file

--- index.js
+++ index.js
@@ -1,40 +1,117 @@
 const axios = require("axios");
-var inquirer = require("inquirer");
-var htmlPage = require("./generateHTML");
+const inquirer = require("inquirer");
 const fs = require("fs");
-const PDFDocument = require('pdfkit');
-const doc = new PDFDocument();
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "username",
-            message: "Enter your Github username"
-        },
-        {
-            type: "input",
-            name: "color",
-            message: "What is your favorite color?"
-        }
-    ]);
-    function writeToFile(doc) {
-        axios.get(`https://api.github.com/users/${username}`).then(function (res) {
-            const picture = res.data.avatar_url;
-            const name = res.data.name;
-            const location = res.data.location;
-            const gitHub = res.data.html_url;
-            const blog = res.data.blog;
-            const bio = res.data.bio;
-            const repoNumber = res.data.public_repos;
-            const followers = res.data.followers;
-            const following = res.data.following;
-            console.log(picture)
-            console.log(location)
-        });
+inquirer
+    .prompt([{
+        message: "Enter GitHub username",
+        name: "username"
+    },
+    {
+        message: "What is your favorite color?",
+        name: "color"
+    }])
+    .then(function ({ username, color }) {
+        const queryUrl = `https://api.github.com/users/${username}`;
 
-    };
-    function init() {
+        axios.get(queryUrl)
+            .then(function (response) {
+                const picUrl = response.data.avatar_url;
+                const location = response.data.location;
+                const gitHubUrl = response.data.html_url;
+                const blog = response.data.blog;
+                const bio = response.data.bio;
+                const numRepos = response.data.public_repos;
+                const numFollowers = response.data.followers;
+                const numFollowing = response.data.following;
+                let numStarred = 0;
 
-        init();
-    }
+
+                axios.get(`https://api.github.com/users/${username}/starred`)
+                    .then(function (response) {
+                        numStarred = response.data.length;
+                        const newHtml = `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>${username}'s Resume</title>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            </head>
+            <body>
+                <div class="container">
+                    <div class="row my-5">
+                        <div class="col-12">
+                            <div class="card text-center">
+                                <img src="${picUrl}" class="card-img-top mx-auto rounded-circle border border-warning" alt="Profile picture" style="width: 18rem;">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 36px;">${username}</p>
+                                    <p class="card-text" style="font-size: 24px;">${bio}</p>
+                                    <a class="card-link" href="https://google.com/maps/place/${location}">${location}</a>
+                                    <a class="card-link" href="${gitHubUrl}">GitHub</a>
+                                    <a class="card-link" href="${blog}">Blog</a>
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Public Repositories</p>
+                                    <p class="card-text" style="font-size: 20px;">${numRepos}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Followers</p>
+                                    <p class="card-text" style="font-size: 20px;">${numFollowers}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">GitHub Stars</p>
+                                    <p class="card-text" style="font-size: 20px;">${numStarred}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card text-center">
+                                <div class="card-body">
+                                    <p class="card-text" style="font-size: 24px;">Following</p>
+                                    <p class="card-text" style="font-size: 20px;">${numFollowing}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <style type="text/css">
+                    body {
+                    background-color: white;
+                    -webkit-print-color-adjust: exact !important;
+                    }
+                    .card {
+                        background-color: ${color};
+                        color: white;
+                    }
+                    .card-link {
+                        color: white;
+                    }
+                </style>
+            </body>
+            </html>`;
+
+
+                        fs.writeFile("profile.html", newHtml, function (err) {
+                            if (err) throw err;
+                        });
+                    });
+            });
+    });
